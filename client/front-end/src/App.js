34f3138e3d7614aa90494d1820dcc8e8e4ba4558
hannahkendall04 <hbkendall@outlook.com>
2023-11-03 13:00:12 -0700

    Added logo and styling

--- client/front-end/src/App.js
+++ client/front-end/src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+    <h1 className="title">StudyNest</h1>
+      <img src={logo} alt="logo" className="home-logo"/>
+    </body>
+
+  
   );
 }
 
