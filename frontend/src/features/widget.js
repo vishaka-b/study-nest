85fa44b7676c0c531059d7e5f844e98d0f21deb8
joy-y-cheng <joycheng@ucla.edu>
2023-11-13 17:33:58 -0800

    Update widget with basic information

--- frontend/src/features/widget.js
+++ frontend/src/features/widget.js
@@ -2,8 +2,9 @@ export default function Widget({name, course, time}) {
     return (
         <div className="widget">
             <h1>{name}</h1>
-            <h2>Course: {course}</h2>
-            <h2>Meeting time: {time}</h2>
+            <h3>Course: {course}</h3>
+            <h3>Meeting time: {time}</h3>
+            <button>More</button>
         </div>
     )
 }
