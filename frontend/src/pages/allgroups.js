6f5158fdf05be922f33c780eecdf5e201ba264e9
Vishaka Bhat <vishakabhat@g.ucla.edu>
2023-11-25 12:07:07 -0800

    Added search feature for group, owner, and course name across all users

--- frontend/src/pages/allgroups.js
+++ frontend/src/pages/allgroups.js
@@ -7,30 +7,71 @@ import React, { useEffect, useState } from 'react';
 export default function AllGroups(){
 
     const [groupsYoureIn, setGroupsYoureIn] = useState("");
+    const [searchText, setSearchText] = useState('');
+    const [filteredGroups, setFilteredGroups] = useState([]);
 
     const getGroupsYoureIn=async()=>{
         const response=await Axios.get("http://localhost:5000/mygroupslist");
         setGroupsYoureIn(response.data);
+        setFilteredGroups(response.data);
         console.log(response.data)
     }
+    
+
+    const handleSearch = () => {
+      const filtered = groupsYoureIn.filter((group) => {
+        return (
+        
+          group.groupName.toLowerCase().includes(searchText.toLowerCase()) ||
+          group.ownersName.toLowerCase().includes(searchText.toLowerCase()) ||
+          group.courseName.toLowerCase().includes(searchText.toLowerCase())
+        );
+      });
+      setFilteredGroups(filtered);
+    };
+
+    const handleShowAllGroups = () => {
+      setSearchText('');
+      setFilteredGroups(groupsYoureIn);
+      console.log(groupsYoureIn);
+
+    };
 
     useEffect(()=> {
         getGroupsYoureIn()
     }, []);
     const [groupsYouveMade, setGroupsYouveMade] = useState("");
+
+    const handleKeyPress = (event) => {
+      if (event.key === 'Enter') {
+        handleSearch();
+      }
+    };
     
     return (
     <div className="allgroups">
     <NavBar />
     <h1>All Groups</h1>
-    <p> List of all groups ever made</p>
+    <p> List of all groups</p>
+
     <div>
-    {Array.isArray(groupsYoureIn) &&
-  groupsYoureIn.map((group, index) => (
+        <input
+          type="text"
+          placeholder="Search for group, course, or owner..."
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+          onKeyPress={handleKeyPress}
+        />
+        <button onClick={handleSearch}>Search</button>
+        <button onClick={handleShowAllGroups}>Show All Groups</button>
+      </div>
+
+    <div>
+    {Array.isArray(filteredGroups) &&
+  filteredGroups.map((group, index) => (
     <h3 key={index}>{group.groupName}</h3>
   ))}
     </div>
    
-    <Widget imageUrl={'./historynew.avif'} groupName={'HIST 3, Lec 2'}/>
     </div>)
 }
