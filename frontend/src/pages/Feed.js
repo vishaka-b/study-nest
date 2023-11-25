ce21d3f7897fc6dada6b3a69c5f99c2c2e14f6ae
joy-y-cheng <joycheng@ucla.edu>
2023-11-25 12:46:37 -0800

    Create modal popup feature

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -3,22 +3,70 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
+import Modal from 'react-bootstrap/Modal';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
 
-function Widget({name, course, time}) {
+function MoreModal(props) {
     return (
-        <Card className="widget">
+        <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    {props.name}
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <h4>{props.course}</h4>
+                <p>
+                    Meeting days: {props.days}
+                    <br />
+                    Meeting time: {props.time}
+                    <br />
+                    Creator: {props.creator}
+                    <br />
+                    Members: {props.members}
+                </p>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button>Join</Button>
+            </Modal.Footer>
+        </Modal>
+    );
+}
+
+function Widget(props) {
+    const [modalShow, setModalShow] = React.useState(false);
+    return (
+        <>
+            <MoreModal
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+                name={props.name}
+                course={props.course}
+                days={props.days}
+                time={props.time}
+            />
+            <Card className="widget">
             <Card.Img variant="top" src="holder.js/100px160"/>
             <Card.Body>
-                <Card.Title>{name}</Card.Title>
+                <Card.Title>{props.name}</Card.Title>
                 <Card.Text>
-                Course: {course}
+                Course: {props.course}
                 <br />
-                Meeting time: {time}
+                Meeting days: {props.days}
                 </Card.Text>
-                <Button variant="primary">More</Button>
+                
+                <Button variant="primary" onClick={() => setModalShow(true)}>More</Button>
+                
             </Card.Body>
-        </Card>
+            </Card>
+        </>
+        
         /*
         <div className="widget">
             <h1>{name}</h1>
@@ -35,7 +83,7 @@ export default function Feed() {
     <Container>
         <Row xs={1} md={2} lg={3}>
             <Col>
-                <Widget name={"Code Crackin' Chicks"} course={"COM SCI 35L"} time={"Mon 5:00 PM"}></Widget>
+                <Widget name={"Code Crackin' Chicks"} course={"COM SCI 35L"} days={["Mon", "Tues"]} time={"5:00 PM"}></Widget>
             </Col>
             <Col>
                 <Widget name={"Tiny Icebergs"} course={"COM SCI 31"} time={"Tue 8:00 PM"}></Widget>
